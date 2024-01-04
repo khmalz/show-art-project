8e6566fecb4eede6289ea69e433f98c3f8a5cddd
khmalz <akmalrma1710@gmail.com>
2024-01-04 12:13:40 +0700

    add: register system

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,38 +7,45 @@
                 <div class="card-body p-4">
                     <h1>Register</h1>
                     <p class="text-medium-emphasis">Create your account</p>
-                    <div class="input-group mb-3"><span class="input-group-text">
-                            <svg class="icon">
-                                <use xlink:href="assets/admin/vendors/@coreui/icons/svg/free.svg#cil-user">
-                                </use>
-                            </svg></span>
-                        <input class="form-control" type="text" placeholder="Username">
-                    </div>
-                    <div class="input-group mb-3"><span class="input-group-text">
-                            <svg class="icon">
-                                <use xlink:href="assets/admin/vendors/@coreui/icons/svg/free.svg#cil-envelope-open">
-                                </use>
-                            </svg></span>
-                        <input class="form-control" type="text" placeholder="Email">
-                    </div>
-                    <div class="input-group mb-3"><span class="input-group-text">
-                            <svg class="icon">
-                                <use xlink:href="assets/admin/vendors/@coreui/icons/svg/free.svg#cil-lock-locked">
-                                </use>
-                            </svg></span>
-                        <input class="form-control" type="password" placeholder="Password">
-                    </div>
-                    <div class="input-group mb-4"><span class="input-group-text">
-                            <svg class="icon">
-                                <use xlink:href="assets/admin/vendors/@coreui/icons/svg/free.svg#cil-lock-locked">
-                                </use>
-                            </svg></span>
-                        <input class="form-control" type="password" placeholder="Repeat password">
-                    </div>
-                    <div class="mb-3">
-                        <p>Already have an account? <a href="{{ route('login') }}">Login</a></p>
-                    </div>
-                    <button class="btn btn-block btn-success" type="button">Register</button>
+                    <form action="{{ route('register') }}" method="POST">
+                        @csrf
+                        <div class="input-group mb-3"><span class="input-group-text">
+                                <svg class="icon">
+                                    <use xlink:href="assets/admin/vendors/@coreui/icons/svg/free.svg#cil-user">
+                                    </use>
+                                </svg></span>
+                            <input class="form-control" name="name" type="name" placeholder="Name" autocomplete="name"
+                                autofocus />
+                        </div>
+                        <div class="input-group mb-3"><span class="input-group-text">
+                                <svg class="icon">
+                                    <use xlink:href="assets/admin/vendors/@coreui/icons/svg/free.svg#cil-envelope-open">
+                                    </use>
+                                </svg></span>
+                            <input class="form-control" name="email" type="email" placeholder="Email"
+                                autocomplete="email" />
+                        </div>
+                        <div class="input-group mb-3"><span class="input-group-text">
+                                <svg class="icon">
+                                    <use xlink:href="assets/admin/vendors/@coreui/icons/svg/free.svg#cil-lock-locked">
+                                    </use>
+                                </svg></span>
+                            <input class="form-control" name="password" type="password" placeholder="Password"
+                                autocomplete="new-password" />
+                        </div>
+                        <div class="input-group mb-4"><span class="input-group-text">
+                                <svg class="icon">
+                                    <use xlink:href="assets/admin/vendors/@coreui/icons/svg/free.svg#cil-lock-locked">
+                                    </use>
+                                </svg></span>
+                            <input class="form-control" name="password_confirmation" type="password" placeholder="Password"
+                                autocomplete="new-password" />
+                        </div>
+                        <div class="mb-3">
+                            <p>Already have an account? <a href="{{ route('login') }}">Login</a></p>
+                        </div>
+                        <button class="btn btn-block btn-success" type="button">Register</button>
+                    </form>
                 </div>
             </div>
         </div>
